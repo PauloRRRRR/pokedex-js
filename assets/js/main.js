cdd0a3f6c090198ab162a773ad0f65f1a3a7aae0
Paulo Ricardo <notadead5@gmail.com>
2022-12-15 22:25:37 -0300

    Promises and API requisition

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => {
+        return response.json()
+    })
+    then((responseBody) => {
+        console.log(responseBody)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+    .finally(() => {
+        console.log('Requisição concluída')
+    })
