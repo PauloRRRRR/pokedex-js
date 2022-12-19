e54d99befb83124e7679c869cc6e9fadf1337acf
Paulo Ricardo <notadead5@gmail.com>
2022-12-19 02:23:53 -0300

    Final commit without pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,33 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0;
 
-function convertPokemonToHtml(pokemon){
-    return `
-        <li class="pokemon">
-            <span class="number">#001</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
-                </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+// mapeando cada item e unindo ao HTML com join
+
+function loadPokemonItems(offset, limit){
+
+    function convertPokemonToHtml(pokemon){
+        return `
+            <li class="pokemon ${pokemon.type}" >
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}" >${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        `
+    }
+    
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+    })
 }
 
-// Consumindo a API com fetch e retornando um array de pokemon
-
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            console.log(convertPokemonToHtml(pokemon))
-            
-            document.getElementsByClassName('pokemons')
-        }
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+loadMoreButton.addEventListener('click', () => {
+    loadPokemonItems()
+})
     
